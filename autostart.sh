7ec6a451b907488c66b80af7facfb20dd1c25d2f
corytertel <ctertel@comcast.net>
2021-09-22 21:52:54 -0700

    first commit

---
+++ autostart.sh
@@ -0,0 +1,17 @@
+# DWM Commands
+
+# DWM Status
+~/.dwm/dwm-bar/dwm_bar.sh &
+
+# Compositor
+picom -f &
+
+# Wallpaper
+feh --bg-fill $HOME/.dwm/the_n9n.png
+
+# Touchpad config
+#xinput --set-prop "DLL096D:01 06CB:CDE6 Touchpad" "Synaptics Tap Time" 0
+#xinput --set-prop "DLL096D:01 06CB:CDE6 Touchpad" "Synaptics Palm Detection" 1
+
+# Notification daemon
+dunst &
